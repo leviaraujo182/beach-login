4d57140e877745e4caa6685291a661aa886d1693
leviaraujo182 <31797494+leviaraujo182@users.noreply.github.com>
2021-08-13 09:35:58 -0300

    Animate Added

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { LoginPage } from 'pages/LoginPage'
 import { CreateAccount } from 'pages/CreateAccount'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export const Routes : React.FC = () => {
     return (
-       <BrowserRouter>
-            <Switch>
-                <Route path="/" component={LoginPage} exact/>
-                <Route path="/createaccount" component={CreateAccount} />
-            </Switch>
-       </BrowserRouter>
+       <Router>
+           <Route render={({ location }) => (
+               <AnimatePresence exitBeforeEnter initial={false}>
+                <Switch location={location} key={location.pathname}>
+                    <Route exact path="/" component={LoginPage} />
+                    <Route exact path="/createaccount" component={CreateAccount} />
+                </Switch>
+               </AnimatePresence>
+           )} />
+       </Router>
     )
 }
--- src/pages/CreateAccount/CreateAccount.tsx
+++ src/pages/CreateAccount/CreateAccount.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Slide from '@material-ui/core/Slide'
 import { Box } from '@material-ui/core'
+import { motion } from 'framer-motion'
 
 export const CreateAccount : React.FC = () => {
+    const transition = {
+        duration: 1,
+        ease: [0.43, 0.13, 0.23, 0.96]
+      };
     return (
-        <Slide direction="left" in={true} mountOnEnter unmountOnExit>
-            <Box>
+        <motion.div animate={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: '100%' }} transition={{  duration: 0.3 }} exit={{ x: '100%'}}>
+            <Box style={{ width: '100%', height: '100vh', backgroundColor: 'red' }}>
                 CreateAccount
             </Box>
-        </Slide>
+        </motion.div>
+     
         
     )
 }
